1093fc647f790c268b8ac1bae1c8db8ee975f320
PrimEnd <ahmed25talbi@gmail.com>
2025-11-02 21:58:51 +0300

    Done

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite{
 		}
 		this.color= color
 		this.isAttacking
+		this.health = 100
 	}
 
 	draw(){
@@ -153,7 +154,8 @@ function animate(){
 	 player.isAttacking
 	 ){
 	 	player.isAttacking = false
-		console.log('go');
+		enemy.health -= 20
+		document.querySelector('#enemyHealth').style.width = enemy.health + '%'
 	}
 
 	if( rectangularCollision({
@@ -163,7 +165,8 @@ function animate(){
 	 enemy.isAttacking
 	 ){
 	 	enemy.isAttacking = false
-		console.log('enemy attack successful');
+		player.health -= 20
+		document.querySelector('#playerHealth').style.width = player.health + '%'
 	}
 }
 animate()
@@ -198,7 +201,7 @@ window.addEventListener('keydown', (event) => {
 			enemy.velocity.y = -20
 			break
 		case 'ArrowDown' :
-			enemy.isAttacking = true
+			enemy.attack()
 			break
 	}
 	// console.log(event.key)
