5c36f61cfe7e8eca3bb3f845da9616f0b694873d
maryam salman <maryamsalmanbk@gmail.com>
2025-11-09 02:07:10 +0300

    Player Sprite - Ide + run

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,12 @@
 class Sprite{
-	constructor({position , imageSrc, scale = 1, framesMax = 1 }) {
+	constructor({
+		position, 
+		imageSrc, 
+		scale = 1, 
+		framesMax = 1, 
+		offset = {x: 0, y:0},
+		sprites
+	}) {
 		this.position = position
 		this.width= 50
 		this.height= 150
@@ -10,7 +17,7 @@ class Sprite{
 		this.framesCurrent = 0
 		this.framesElapsed = 0
 		this.framesHold = 5
-
+		this.offset = offset
 	}
 
 	draw(){
@@ -20,31 +27,50 @@ class Sprite{
 			0,
 			this.image.width / this.framesMax,
 			this.image.height,
-			this.position.x,
-			this.position.y, 
+			this.position.x - this.offset.x,
+			this.position.y - this.offset.y, 
 			 (this.image.width / this.framesMax)* this.scale,
 			this.image.height * this.scale
 		)
     }
+
+	animateFrames() {
+		this.framesElapsed++
+
+	 if (this.framesElapsed % this.framesHold === 0){
+       if (this.framesCurrent < this.framesMax - 1) {
+         this.framesCurrent++
+        } else {
+	      this.framesCurrent = 0
+        }  
+      }
+	}
 	
 	update(){
      this.draw()
-	 this.framesElapsed++
-
-	 if (this.framesElapsed % this.framesHold === 0){
-
-	 if (this.framesCurrent < this.framesMax - 1) {
-     this.framesCurrent++
-    } else {
-	 this.framesCurrent = 0
-    }  
-   }
-  }
+	 this.animateFrames()
+    }
 }
 
-class Fighter{
-	constructor({position,velocity, color = 'red', offset}) {
-		this.position = position
+class Fighter extends Sprite {
+	constructor({
+		position, 
+		velocity, 
+		color = 'red',  
+		imageSrc, 
+		scale = 1, 
+		framesMax = 1,
+		offset = { x: 0, y:0 },
+		sprites
+	}) {
+		super( {
+			position,
+			imageSrc,
+			scale,
+			framesMax,
+			offset
+		})
+
 		this.velocity= velocity
 		this.width= 50
 		this.height= 150
@@ -61,36 +87,32 @@ class Fighter{
 		this.color= color
 		this.isAttacking
 		this.health = 100
+		this.framesCurrent = 0
+		this.framesElapsed = 0
+		this.framesHold = 5
+		this.sprites = sprites
+		
+		for (const sprite in this.sprites) {
+			sprites[sprite].image = new Image()
+			sprites[sprite].image.src = sprites[sprite].imageSrc
+		}
 	}
 
-	draw(){
-		 c.fillStyle = this.color	
-		 c.fillRect(this.position.x , this.position.y, this.image.width, this.image.height)
-		 // attack box
-		 if(this.isAttacking){
-		 c.fillStyle = 'green'
-		 c.fillRect(
-		 	this.attackBox.position.x,
-		 	this.attackBox.position.y,
-		 	this.attackBox.width,
-		 	this.attackBox.height
-		 	)
-		  }
-		}
-	
 	update(){
      this.draw()
+	 this.animateFrames()
+
      this.attackBox.position.x = this.position.x + this.attackBox.offset.x
      this.attackBox.position.y = this.position.y
+
      this.position.x += this.velocity.x
      this.position.y += this.velocity.y
 
-     if (this.position.y + this.height + this.velocity.y >= 
-        canvas.height -96
-    ){
+     if (this.position.y + this.height + this.velocity.y >= canvas.height -96 ) {
      	this.velocity.y = 0
-     }else this.velocity.y += gravity
+     } else this.velocity.y += gravity
   }
+
   attack(){
   	this.isAttacking = true
   	setTimeout(() => {
